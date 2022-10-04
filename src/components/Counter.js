1ee1af200eed19791962e34cf4ac1f9ae528bb0c
amosmaundundu <amosmaundu5@gmail.com>
2022-10-04 15:38:59 +0300

    take-2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
 
+import React, { useState } from "react";
+
 function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
     // console.log(`before setState: ${count}`);
 
-    setCount((currentCount)=>currentCount + 1);
-    setCount((currentCount)=>currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+
+    // setCount(count + 1);
+    // console.log(count)
+
+    // setCount(count + 1);
+    // console.log(count)
 
     // console.log(`after setState: ${count}`);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={increment}>{count}</button>;
 }
 
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     // console.log(`before setState: ${count}`);
+
+//     setCount((currentCount)=>currentCount + 1);
+//     setCount((currentCount)=>currentCount + 1);
+
+//     // console.log(`after setState: ${count}`);
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
+
 export default Counter;
+
+ 
